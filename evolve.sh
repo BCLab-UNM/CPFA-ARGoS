bbef3d4adffc730814eb28c7358b47236ea054d9
Matthew Fricke <matthew@fricke.co.uk>
2016-02-16 19:15:11 -0700

    updated eveolve script to not use MPI and email at end

--- evolve.sh
+++ evolve.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-nohup mpirun -n $1 -machinefile $2 ~/GitHub/CPFA-ARGoS/build/cpfa_evolver -t 1 -p 100 -g 100 -c 0.1 -m 0.1 -s 0.1 &
+rm nohup.out
+#nohup bash -c "mpirun -n $1 -machinefile $2 ~/GitHub/CPFA-ARGoS/build/cpfa_evolver -t 15 -p 50 -g 100 -c 0.1 -m 0.1 -s 1.0 -e 1 && cat evolution.txt | mail -s \"CPFA Evolution Report\" matthew@fricke.co.uk" &
+nohup bash -c "~/GitHub/CPFA-ARGoS/build/cpfa_evolver -t 15 -p 50 -g 100 -c 0.1 -m 0.1 -s 1.0 -e 1 && cat evolution.txt | mail -s \"CPFA Evolution Report\" matthew@fricke.co.uk" &
