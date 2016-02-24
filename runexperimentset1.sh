d344f403bcb830ef8beb70b39f4924697bfa89a9
Matthew Fricke <matthew@philautia.cs.unm.edu>
2016-02-23 22:26:32 -0700

    Fixed experiment script bugs

--- runexperimentset1.sh
+++ runexperimentset1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 1 2 4 6 8 10 15 20 25 30
 do
-    ./runntimes.sh $1 experiments/CPFAPL${i}Searcher.xml results/CPFAPL${i}Searchers.txt
+    ./runntimes.sh $1 experiments/CPFAPL${i}Searchers.xml results/CPFAPL${i}Searchers.txt
 done
--- runexperimentset4.sh
+++ runexperimentset4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 10 12 14 16 18 20
 do
-    ./runntimes.sh $1 experiments/CPFAU60minSize${i}x${i}.xml results/CPFAU60minSize${i}x${i}.txt
+    ./runntimes.sh $1 experiments/CPFAU60minArea${i}x${i}.xml results/CPFAU60minArea${i}x${i}.txt
 done
