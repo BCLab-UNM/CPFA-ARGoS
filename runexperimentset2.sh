28371667fff19734d3e31844a5e914458b254cb6
Matthew Fricke <matthew@fricke.co.uk>
2016-02-23 21:58:09 -0700

    Bug fix: experiment scripts wrong output path

--- runexperimentset2.sh
+++ runexperimentset2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 0 1 2 3 4 5 6 7 8 9 10
 do
-    ./runntimes.sh 25 experiments/CPFAPLError${i}.xml final_results/CPFAPLError${i}.txt
+    ./runntimes.sh 25 experiments/CPFAPLError${i}.xml results/CPFAPLError${i}.txt
 done
