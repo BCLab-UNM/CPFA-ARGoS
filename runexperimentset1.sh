9fbefda2a87938f96cac6dfb9f938f26714abe24
Matthew Fricke <matthew@fricke.co.uk>
2016-02-23 21:44:36 -0700

    New experiment scripts for distributing across machines

---
+++ runexperimentset1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+    ./runntimes.sh 25 experiments/CPFAPL${i}Searchers.xml final_results/CPFAPL${i}Searchers.txt &
+done
---
+++ runexperimentset3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in 0 1 2 3 4 5 6 7 8 9 10
+do
+    ./runntimes.sh 25 experiments/CPFACError${i}.xml final_results/CPFACError${i}.txt &
+done
