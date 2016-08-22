cd5596a456bcc51cc2603f02ae953d9a909d8124
Matthew Fricke <mfricke@cs.unm.edu>
2016-08-22 15:37:35 -0600

    Added a comment to the cpfa evolver sample script This comment explains that the second argument selects the MPI machinefile name.

--- evolve_EXAMPLE.sh
+++ evolve_EXAMPLE.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # First argument is the number of processes to generate
+# Second argumen is the name of the machine file
 
 nohup bash -c "mpirun -n $1 -machinefile $2 ~/GitHub/CPFA-ARGoS/build/cpfa_evolver -t 10 -p 50 -g 100 -c 0.1 -m 0.1 -s 0.2 -e 1 -x experiments/ExampleEvolution.xml seed 1111 && cat example_evolution_output.txt | mail -s \"Example Evolution Description\" example@domain.com" > evolve_clustered.out &
 
